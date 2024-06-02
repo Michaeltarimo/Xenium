548c88416478a3a838ba4cad224c5fe6fecb7d9a
Michael Tarimo, MD, Dev <56628755+Michaeltarimo@users.noreply.github.com>
2024-06-02 05:01:40 +0000

    updated code

--- src/pages/index.js
+++ src/pages/index.js
@@ -29,9 +29,10 @@ const index = () => {
               <h1 className="text-sm font-light text-gray-400">( 1 batch = 350000000 XEN )</h1>
             </div>
 
-            <div className="flex flex-col items-center justify-start p-5 rounded-lg bg-blue-400/20 w-[85%] mt-5 h-[150px]">
+            <div className="flex flex-col border border-gray-300 items-center justify-start p-5 rounded-lg bg-blue-400/20 w-[85%] mt-5 h-[200px]">
               <h1 className="text-xs font-medium text-gray-300">350,000,000.00 XEN</h1>
               <h1 className="text-[10px] text-gray-300 font-medium">Xen to burn</h1>
+              <div className="border2"/>
               <div className="flex items-center justify-center">
 
                 <nav className="flex rounded-l-sm hover:bg-gray-400/50 cursor-pointer transition transition-transform duration-300 hover:scale-105 text-lg font-extrabold text-red-500 mt-4 items-center justify-center border border-gray-400 w-[40px] h-[40px]">
@@ -47,7 +48,7 @@ const index = () => {
               </div>
             </div>
 
-            <div className="flex flex-col items-start justify-start p-5 rounded-lg bg-blue-400/20 w-[85%] mt-5 h-[100px]">
+            <div className="flex flex-col items-start justify-start p-5 rounded-lg bg-blue-400/20 w-[85%] mt-5 h-[150px]">
             </div>
 
             <button className="flex items-center justify-center transition transition-transform duration-500 hover:scale-105 font-bold text-xs py-3 rounded-3xl border border-gray-400 bg-black mt-5 w-[85%]">
