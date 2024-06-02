6cdf98fbb1fec26bf8bf0fb52c168d491a67c4b0
Michael Tarimo, MD, Dev <56628755+Michaeltarimo@users.noreply.github.com>
2024-06-02 12:24:40 +0000

    updated components

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,7 +15,7 @@ const index = () => {
 
         <div className="flex items-start flex-row justify-start w-full min-h-screen md:hidden">
 
-          <div className="flex py-2 flex-col items-center justify-start bg-darkest h-screen w-1/4 rounded-b-xl">
+          <div className="flex py-2 flex-col items-center justify-start bg-darkest h-auto w-1/4 rounded-b-xl">
 
             <nav className="w-full flex items-center justify-center">
               <div>
@@ -29,22 +29,28 @@ const index = () => {
               <h1 className="text-sm font-light text-gray-400">( 1 batch = 350000000 XEN )</h1>
             </div>
 
-            <div className="flex flex-col border border-gray-300 items-center justify-start pt-8 rounded-lg bg-blue-400/20 w-[85%] mt-5 h-[200px]">
-              <h1 className="text-xs font-medium text-gray-300">350,000,000.00 XEN</h1>
-              <h1 className="text-[10px] text-gray-300 font-medium">Xen to burn</h1>
+            <div className="flex flex-col border border-gray-300 items-center justify-start pt-10 rounded-lg bg-blue-400/20 w-[85%] mt-5 h-[200px]">
+              <h1 className="text-xs font-bold text-gray-300">350,000,000.00 XEN</h1>
+              <h1 className="text-[10px] text-gray-300 font-bold">Xen to burn</h1>
 
               <div className="flex items-center justify-center mt-4 w-full">
-                <nav className="flex rounded-l-sm hover:bg-gray-400/50 cursor-pointer transition transition-transform duration-300 hover:scale-105 text-lg font-extrabold text-red-500 items-center justify-center border border-gray-400 w-[60px] h-[50px]">
+                <nav className="flex font-bold rounded-l-sm hover:bg-gray-400/50 cursor-pointer transition transition-transform duration-300 hover:scale-105 text-lg font-extrabold text-red-500 items-center justify-center border border-gray-400 w-[60px] h-[50px]">
                   <h1 className="cursor-pointer">-</h1>
                 </nav>
-                <nav className="flex text-xs items-center justify-center border-t border-b border-gray-400 w-full h-[50px]">
+                <nav className="flex text-xs font-bold items-center justify-center border-t border-b border-gray-400 w-full h-[50px]">
                   00000000
                 </nav>
-                <nav className="flex rounded-r-sm hover:bg-gray-400/50 cursor-pointer transition transition-transform duration-300 hover:scale-105 text-lg font-extrabold text-green-500 items-center justify-center border border-gray-400 w-[60px] h-[50px]">
+                <nav className="flex font-bold rounded-r-sm hover:bg-gray-400/50 cursor-pointer transition transition-transform duration-300 hover:scale-105 text-lg font-extrabold text-green-500 items-center justify-center border border-gray-400 w-[60px] h-[50px]">
                   <h1 className="">+</h1>
                 </nav>
 
               </div>
+
+              <div className="flex text- font-bold px-2 mt-5 w-full items-center justify-between">
+                <h1>Fee:</h1>
+                <h1>-30855 PLS</h1>
+              </div>
+
             </div>
 
             <div className="flex flex-col items-start justify-start p-5 rounded-lg bg-blue-400/20 w-[85%] mt-5 h-[150px]">
